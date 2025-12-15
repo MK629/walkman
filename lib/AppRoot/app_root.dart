a2d04ec2bb51e3fc71343926fc0ef1c1876b69d0
mk <myatkw629@gmail.com>
2025-12-16 00:19:49 +0630

    init-2

---
+++ lib/AppRoot/app_root.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+  
+}
